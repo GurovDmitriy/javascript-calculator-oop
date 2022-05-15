d5450cacc255d0c74be7932fb2650cd0b262a5dd
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-05-15 17:28:24 +0300

    feat: end base functional

--- src/calculatorUI.js
+++ src/calculatorUI.js
@@ -11,8 +11,57 @@ export default class CalculatorUI {
     this._initBtnClear()
     this._initBtnDot()
     this._initBtnResult()
+    this._initKeyup()
 
     this._initDisplay = this._initDisplay()
+  }
+
+  _initKeyup() {
+    document.addEventListener("keyup", (evt) => {
+      const key = evt.key
+
+      const operation = {
+        "+": "plus",
+        "-": "minus",
+        "*": "multiple",
+        "/": "divide",
+      }
+
+      switch (key) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+          this._pushBtnNum(Number(key))
+          break
+
+        case "+":
+        case "-":
+        case "/":
+        case "*":
+          this._pushBtnOperation(operation[key])
+          break
+
+        case "=":
+          this._pushBtnResult()
+          break
+
+        case "Backspace":
+        case "Delete":
+          this._pushBtnClear()
+          break
+
+        case ".":
+          this._pushBtnDot()
+          break
+      }
+    })
   }
 
   _initBtnNum() {
@@ -63,7 +112,7 @@ export default class CalculatorUI {
 
   _initDisplay() {
     return {
-      operations: document.querySelector(".calculator__operations"),
+      operation: document.querySelector(".calculator__operations"),
       result: document.querySelector(".calculator__result"),
       error: document.querySelector(".calculator__error"),
     }
@@ -78,8 +127,10 @@ export default class CalculatorUI {
 
       if (this.calculator.action) {
         this._initDisplay.result.textContent = this.calculator.numbers.numB
+        this._setDisplayOperation()
       } else {
         this._initDisplay.result.textContent = this.calculator.numbers.numA
+        this._setDisplayOperation()
       }
     } catch (err) {
       this._initDisplay.error.textContent = err.message
@@ -90,9 +141,19 @@ export default class CalculatorUI {
     try {
       this._initDisplay.error.textContent = "no error"
 
+      const actionText = {
+        plus: "+",
+        minus: "-",
+        divide: "/",
+        multiple: "*",
+      }
+
       if (this._checkBtnOperation()) {
         this.calculator.action = value
-        this._initDisplay.result.textContent = this.calculator.action
+        this._initDisplay.result.textContent =
+          actionText[this.calculator.action]
+
+        this._setDisplayOperation()
       }
     } catch (err) {
       this._initDisplay.error.textContent = err.message
@@ -107,9 +168,8 @@ export default class CalculatorUI {
         this.calculator.isCalculateResult = true
         const result = this.calculator.result()
 
-        console.log(this.calculator)
-
         this._initDisplay.result.textContent = result
+        this._initDisplay.operation.textContent += ` = ${result}`
       }
     } catch (err) {
       this._initDisplay.error.textContent = err.message
@@ -122,6 +182,7 @@ export default class CalculatorUI {
 
       this.calculator.numbers.reset()
       this._initDisplay.result.textContent = 0
+      this._initDisplay.operation.textContent = "history"
     } catch (err) {
       this._initDisplay.error.textContent = err.message
     }
@@ -134,18 +195,36 @@ export default class CalculatorUI {
 
       if (this.calculator.action) {
         this._initDisplay.result.textContent = this.calculator.numbers.numB
+        this._setDisplayOperation()
       } else {
         this._initDisplay.result.textContent = this.calculator.numbers.numA
+        this._setDisplayOperation()
       }
     } catch (err) {
       this._initDisplay.error.textContent = err.message
     }
   }
 
+  _setDisplayOperation() {
+    const actionText = {
+      plus: "+",
+      minus: "-",
+      divide: "/",
+      multiple: "*",
+    }
+
+    const a = this.calculator.numbers.numA
+    const b = this.calculator.numbers.numB
+    const d = this.calculator.action ? actionText[this.calculator.action] : ""
+
+    this._initDisplay.operation.textContent = `${a} ${d} ${b}`
+  }
+
   _checkBtnResult() {
     if (!this.calculator.numbers.numA) throw new Error("numA is required")
     if (!this.calculator.numbers.numB) throw new Error("numB is required")
     if (!this.calculator.action) throw new Error("action is required")
+    if (this.calculator._isCalculateResult) return false
 
     return true
   }
@@ -166,6 +245,7 @@ export default class CalculatorUI {
     const a = this.calculator.numbers.numA
     const b = this.calculator.numbers.numB
 
+    if (!a) return false
     if (!b && a.includes(".")) return false
     if (b && b.includes(".")) return false
     if (this.calculator._isCalculateResult) return false
